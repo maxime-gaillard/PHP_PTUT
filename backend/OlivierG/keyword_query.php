096fb3c022d8642f86563973d3d1146cef278071
olivier-goasampis <olivier.goasampis@etu.univ-amu.fr>
2017-10-29 18:17:40 +0100

    Récupération des données du formulaire pour traitement

--- backend/OlivierG/keyword_query.php
+++ backend/OlivierG/keyword_query.php
@@ -8,31 +8,33 @@
 try {
     $bdd = new PDO('mysql:host=mysql-groupe2equipe2ptut.alwaysdata.net;dbname=groupe2equipe2ptut_base;charset=utf8', '146012_olivierg', '123', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
     // array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION) : accurate SQL errors description
+
+    // $req = $bdd->query("SELECT * FROM Publication WHERE LibelleP LIKE '%".$keyword."%'");
+    // This instruction has low security regarding SQL injection, we use prepare and execute instead.
+
+    $req = $bdd->prepare("SELECT * FROM Publication WHERE LibelleP LIKE '%?%'");
+
+    $keyword = htmlspecialchars(isset($_POST['keyword']) ? $_POST['keyword'] : NULL);
+    $req->execute(array($keyword));
+
+    while ($data = $req->fetch()) {
+        ?> <strong>
+
+            <?php
+            echo $data['LibelleP'];
+            ?> </strong><br/>
+
+        <?php
+        echo $data['DescriptionP'];
+        ?> <br/><br/>
+
+        <?php
+    }
+
+    $req->closeCursor();
+    $bdd = null;
 }
-catch (Exception $e) {
-    die($e->getMessage());
+catch (PDOException $e) {
+    $e->getMessage();
 }
-
-// $req = $bdd->query("SELECT * FROM Publication WHERE LibelleP LIKE '%".$keyword."%'");
-// This instruction has low security regarding SQL injection, we use prepare and execute instead.
-
-$req = $bdd->prepare("SELECT * FROM Publication WHERE LibelleP LIKE '%?%'");
-//$req->execute(array(isset($_POST['keyword']) ? $_POST['keyword'] : NULL));
-$req->execute(array($_POST['keyword']));
-
-while($data = $req->fetch()) {
-?> <strong>
-
-<?php
-    echo $data['LibelleP'];
-?> </strong><br />
-
-<?php
-    echo $data['DescriptionP'];
-?> <br/><br/>
-
-<?php
-}
-
-$req->closeCursor();
 ?>
