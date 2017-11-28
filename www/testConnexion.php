45405079bf5d605007448b1f4dcba0b1dfca1a12
fesquet <olivier.fesquet@etu.univ-amu.fr>
2017-11-28 23:21:04 +0100

    La connexion d'un inscrit fonctionne.

--- www/testConnexion.php
+++ www/testConnexion.php
@@ -9,29 +9,26 @@ catch (Exception $e)
     die('Erreur de connexion : ' . $e->getMessage());
 }
 
-$pass_hache = password_hash($_POST['MotDePasse1'], PASSWORD_BCRYPT);
+$req = $bdd->prepare('SELECT NumInscrit, MotDePasse FROM Inscrit WHERE Email = :Email1');
 
-$req = $bdd->prepare('SELECT NumInscrit FROM Inscrit WHERE Email = :Email1');
-
-$req->execute(array('Email1' => $_POST['Email1'],'MotDePasse1' => $pass_hache));
+$req->execute(array('Email1' => $_POST['Email1']));
 
 $resultat = $req->fetch();
 
 if (!$resultat)
 {
-    echo 'Mauvais identifiant !';
+    echo 'Mauvais mail !';
 }
 else
 {
-    if (password_verify($_POST['MotDePasse1'], $pass_hache)) {
-        echo "OK";
+    if (password_verify($_POST['MotDePasse1'], $resultat['MotDePasse'])) {
+        session_start();
+        $_SESSION['NumInscrit'] = $resultat['NumInscrit'];
+        $_SESSION['Email1'] = $_POST['Email1'];
+        echo 'Vous êtes connecté !';
     }
     else {
         echo 'Mauvais mot de passe !';
     }
-    session_start();
-    $_SESSION['NumInscrit'] = $resultat['NumInscrit'];
-    $_SESSION['Email1'] = $Email;
-    echo 'Vous êtes connecté !';
 }
 ?>
