ca15db9431647d55998e576ae22d0703b98152df
fesquet <olivier.fesquet@etu.univ-amu.fr>
2017-11-28 01:06:04 +0100

    La connexion d'un inscrit ne fonctionne pas encore.

--- backend/OlivierF/testConnexion.php
+++ backend/OlivierF/testConnexion.php
@@ -9,17 +9,19 @@ catch (Exception $e)
     die('Erreur de connexion : ' . $e->getMessage());
 }
 
-$pass_hache = password_hash($_POST['MotDePasse'], PASSWORD_DEFAULT);
+    $pass_hache = password_hash($_POST['MotDePasse'], PASSWORD_DEFAULT);
 
 $req = $bdd->prepare('SELECT NumInscrit FROM Inscrit WHERE Email = :Email AND MotDePasse = :MotDePasse');
-$req->execute(array('Email' => $Email,
-                    'MotDePasse' => $MotDePasse));
+
+$req->execute(array('Email' => $_POST['Email'],'MotDePasse' => $pass_hache));
 
 $resultat = $req->fetch();
 
 if (!$resultat)
 {
     echo 'Mauvais identifiant ou mot de passe !';
+    echo '\n';
+    echo $pass_hache;
 }
 else
 {
