4f19d4b827da1237815dd730c5e2369fdd513ca3
g16002495 <maxime.gaillard.1@etu.univ-amu.fr>
2017-11-30 15:51:36 +0100

    Mise en forme formulaire mise en ligne publication

--- www/insription.php
+++ www/insription.php
@@ -15,6 +15,7 @@ try {
 //    $descitpion = htmlspecialchars($_POST['description']);
 //    if($descitpion == "" || is_null($descitpion)) die();
 
+
     if ($type == "chercheur") {
 //        $idChercheur = htmlspecialchars($_POST['idChercheur']);
 //        if($idChercheur == "" || is_null($idChercheur)) die();
