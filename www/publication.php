29ad70a06d4dee576e31d6d28fc163b40fdb1d37
maxime-gaillard <maxime.gaillard.1@etu.univ-amu.fr>
2017-12-02 14:02:24 +0100

    Mise en ligne de publication Ok, reste plus qu'a tester pour un utilisateur connecté.

--- www/publication.php
+++ www/publication.php
@@ -2,59 +2,34 @@
 try {
     $bdd = new PDO('mysql:host=mysql-groupe2equipe2ptut.alwaysdata.net;dbname=groupe2equipe2ptut_base;charset=utf8', '146012_maxime', '123', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
-    $titre = htmlspecialchars($_POST['titre']);
-    if($titre == "" || is_null($titre)) die();
-
-//    $date = htmlspecialchars($_POST['date']);
-//    if($date == "" || is_null($date)) die();
-
     $descitpion = htmlspecialchars($_POST['description']);
     if($descitpion == "" || is_null($descitpion)) die();
 
-//    if ($type == "chercheur") {
-//        $idChercheur = htmlspecialchars($_POST['idChercheur']);
-//        if($idChercheur == "" || is_null($idChercheur)) die();
-
     $id = $_SESSION['NumInscrit'];
-
-    $req1 = $bdd->prepare('SELECT * FROM Chercheur WHERE idC LIKE ?');
-    $req1->execute(array('%' . $id . '%'));
-
-    $resultat = $req->fetch();
-
-    if (!$resultat) {
-        $req = $bdd->prepare('INSERT INTO PublicationPublique (LibelleP, DescriptionP) VALUES (:LibelleP, :Description)');
+	
+	if(is_null($_SESSION['NumInscrit'])) {
+        echo "Vous n'etes pas connecte !";
+        die();
     }
 
-    $req = $bdd->prepare('INSERT INTO Publication (LibelleP, DescriptionP) VALUES (:LibelleP, :Description)');
-    $req->execute(array(
-        'LibelleP' => $_POST['titre'],
-        'Description' => $_POST['description']) ) or die ( print_r($req->errorInfo())) ;
+    $req1 = $bdd->prepare('SELECT * FROM Chercheur WHERE NumC = :numC');
+    $req1->execute(array('numC' => $id,) ) or die ( print_r($req->errorInfo())) ;
+
+    $resultat = $req1->fetch();
+
+    if ($resultat) {
+        $req = $bdd->prepare('INSERT INTO PublicationPublique (LibellePubl, NumInscrit) VALUES (:Libelle, :NumInscrit)');
+		$req->execute(array(
+        'Libelle' => $_POST['description'],
+		'NumInscrit' => $id,) ) or die ( print_r($req->errorInfo())) ;
+    } else {
+		$req = $bdd->prepare('INSERT INTO PublicationPrivee (LibellePriv, NumInscrit) VALUES (:Libelle, :NumInscrit)');
+		$req->execute(array(
+        'Libelle' => $_POST['description'],
+		'NumInscrit' => $id,) ) or die ( print_r($req->errorInfo())) ;
+	}
 
     echo 'Une nouvelle publication a été ajoutée !';
-
-//    } elseif ($type == "entreprise") {
-//        $siren = htmlspecialchars($_POST['siren']);
-//        if($siren == "" || is_null($siren)) die();
-//
-//        $req = $bdd->prepare('INSERT INTO Publication (LibelleP, DescriptionP) VALUES (:LibelleP, :Description)');
-//
-//        $req->execute(array(
-//            'LibelleP' => $_POST['libelle'],
-//            'Description' => $_POST['description']) ) or die ( print_r($req->errorInfo())) ;
-//
-//        echo 'Une nouvelle publication a été ajoutée !';
-//
-//
-//    } elseif ($type == "etudiant") {
-//        $req = $bdd->prepare('INSERT INTO Publication (LibelleP, DescriptionP) VALUES (:LibelleP, :Description)');
-//
-//        $req->execute(array(
-//            'LibelleP' => $_POST['libelle'],
-//            'Description' => $_POST['description']) ) or die ( print_r($req->errorInfo())) ;
-//
-//        echo 'Une nouvelle publication a été ajoutée !';
-//    }
 
     $req->closeCursor();
     $bdd = null;
