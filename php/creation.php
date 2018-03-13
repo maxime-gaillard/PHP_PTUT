59bb048c6f3c421fef12a66ce25e68fe5e79b8f1
oliviergoas <olivier.goasampis@gmail.com>
2018-03-13 19:23:23 +0100

    boilerplate

---
+++ php/creation.php
@@ -0,0 +1,49 @@
+<?php
+session_start();
+try
+{
+    $bdd = new PDO('mysql:host=mysql-groupe2equipe2ptut.alwaysdata.net;dbname=groupe2equipe2ptut_base;charset=utf8', '146012', '123');
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (Exception $e)
+{
+    die('Erreur à la création : ' . $e->getMessage());
+}
+
+$nom = htmlspecialchars($_POST['Nom']);
+$prenom = htmlspecialchars($_POST['Prenom']);
+$email= htmlspecialchars($_POST['Email']);
+$pass_hache = password_hash($_POST['MotDePasse'], PASSWORD_DEFAULT);
+$description = htmlspecialchars($_POST['Description']);
+$type = $_POST['type'];
+
+$req = $bdd->prepare('INSERT INTO Inscrit(Nom, Prenom, Email, MotDePasse, Description) VALUES(:Nom, :Prenom, :Email, :MotDePasse, :Description)');
+
+$req->execute(array(
+    'Nom'         => $nom,
+    'Prenom'      => $prenom,
+    'Email'       => $email,
+    'MotDePasse'  => $pass_hache,
+    'Description' => $description) ) or die ( print_r($req->errorInfo())) ;
+
+$NumInscrit = $bdd -> lastInsertId();
+
+if($type == "chercheur"){
+    $IdC = ($_POST['IdC']);
+    $reqChercheur = $bdd->prepare('INSERT INTO Chercheur(NumC, IdC) VALUES(:NumC, :IdC)');
+    $reqChercheur->execute(array(
+        'NumC' => $NumInscrit,
+        'IdC'  => $IdC)) or die (print_r($req->errorInfo()));
+} else {
+    $NomE = ($_POST['NomE']);
+    $SIREN = ($_POST['SIREN']);
+    $reqEntreprise = $bdd->prepare('INSERT INTO Entreprise(NumE, NomE, SIREN) VALUES(:NumE, :NomE, :SIREN )');
+    $reqEntreprise->execute(array(
+        'NumE'  => $NumInscrit,
+        'NomE'  => $NomE,
+        'SIREN' => $SIREN)) or die (print_r($req->errorInfo()));
+    }
+
+header('Location:index.html');
+$bdd = null;
+?>
---
+++ php/rechercheMotCle.php
@@ -0,0 +1,66 @@
+<?php
+session_start();
+try {
+    $bdd = new PDO('mysql:host=mysql-groupe2equipe2ptut.alwaysdata.net;dbname=groupe2equipe2ptut_base;charset=utf8',
+        '146012_olivierg',
+        '123',
+        array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    // array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION) : accurate SQL errors description
+
+    // $req = $bdd->query("SELECT * FROM Publication WHERE LibelleP LIKE '%".$keyword."%'");
+    // This instruction has low security regarding SQL injection, we use prepare and execute instead.
+
+    $keyword = htmlspecialchars($_POST['search']);
+    if($keyword == "" || is_null($keyword)) {
+        echo "Valeur de recherche nulle !";
+        die();
+    }
+
+    if(is_null($_SESSION['NumInscrit'])) {
+        echo "Vous n'etes pas connecte !";
+        die();
+    }
+
+    $req = $bdd->prepare('SELECT * FROM Entreprise WHERE NumE LIKE ?');
+    $req->execute(array($_SESSION['NumInscrit']));
+
+    if($req->fetch()) {
+        $req = $bdd->prepare('SELECT * FROM PublicationPrivee WHERE LibellePriv = :search');
+        $req->execute(array('search' => $_POST['search']));
+
+        echo "Resultats de la recherche : <br/><br/>";
+        if($req->rowCount() == 0) {
+            echo "Pas de resultat";
+        }
+        else {
+            while ($data = $req->fetch()) {
+                echo "<strong>";
+                echo $data['LibellePriv'];
+                echo "</strong><br/>";
+            }
+        }
+    }
+    else {
+        $req = $bdd->prepare('SELECT * FROM PublicationPublique WHERE LibellePubl = :search');
+        $req->execute(array('search' => $_POST['search']));
+
+        echo "Resultats de la recherche : <br/><br/>";
+        if($req->rowCount() == 0) {
+            echo "Pas de resultat";
+        }
+        else {
+            while ($data = $req->fetch()) {
+                echo "<strong>";
+                echo $data['LibellePubl'];
+                echo "</strong><br/>";
+            }
+        }
+    }
+
+    $req->closeCursor();
+    $bdd = null;
+}
+catch (PDOException $e) {
+    $e->getMessage();
+}
+?>
