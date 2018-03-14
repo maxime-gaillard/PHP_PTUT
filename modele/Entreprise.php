9db9d52ab3c471ff093dd134ce7b4bae71efcb15
maxime-gaillard <maxime.gaillard.1@etu.univ-amu.fr>
2018-03-14 14:10:39 +0100

    boilerplate

--- modele/Entreprise.php
+++ modele/Entreprise.php
@@ -5,3 +5,18 @@
  * Date: 14/03/18
  * Time: 13:37
  */
+
+require_once ('db_connect.php');
+
+class Entreprise
+{
+    public static function insertEntreprise($NumInscrit, $NomE, $SIREN)
+    {
+        $req = $GLOBALS['pdo']->prepare('INSERT INTO Entreprise(NumE, NomE, SIREN) VALUES(:NumE, :NomE, :SIREN )');
+        $req->execute(array(
+            'NumE'  => $NumInscrit,
+            'NomE'  => $NomE,
+            'SIREN' => $SIREN)) or die (print_r($req->errorInfo()));
+    }
+
+}
--- modele/Inscrit.php
+++ modele/Inscrit.php
@@ -5,3 +5,20 @@
  * Date: 14/03/18
  * Time: 13:37
  */
+
+require_once ('db_connect.php');
+
+
+class Inscrit
+{
+    public static function insertInscrit($nom, $prenom, $email, $pass_hache, $description)
+    {
+        $req = $GLOBALS['pdo']->prepare('INSERT INTO Inscrit(Nom, Prenom, Email, MotDePasse, Description) VALUES(:Nom, :Prenom, :Email, :MotDePasse, :Description)');
+        $req->execute(array(
+            'Nom'         => $nom,
+            'Prenom'      => $prenom,
+            'Email'       => $email,
+            'MotDePasse'  => $pass_hache,
+            'Description' => $description) ) or die ( print_r($req->errorInfo())) ;
+    }
+}
