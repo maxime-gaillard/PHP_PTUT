e9a5b697cbc1a409d7c76ce96d80bf42a715c4d9
f16002025 <julien.faou@etu.univ-amu.fr>
2018-03-14 15:56:14 +0100

    MVC

--- js/rssToJson.js
+++ js/rssToJson.js
@@ -1,31 +1,31 @@
-$(document).ready(function() {
+(function() {
 
-    // The helper function to convert XML to JSON
-    function parseRSS(url, callback) {
-        $.ajax({
-            url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=2.0&callback=?&q=' + encodeURIComponent(url),
-            dataType: 'json',
-            success: function (data) {
-                callback(data.responseData.feed);
-            }
-        });
-    }
+    $(document).ready(function() {
 
-    // Get the latest 5 articles from a WordPress site
-    function showArticles(json) {
-        for(let k=0; k<5; ++k)
-        {
-            var container = $(toString('#article'+k)).html(''), dynamicItems = '';
-            $.each(json.entries, function (i, val) {
-                dynamicItems += '<li><a href="' + val.link + '">' + val.title + '</a></li>';
-                return i < 4;
-            });
-            container.append(dynamicItems);
+
+        function capitaliseFirstLetter(string) {
+            return string.charAt(0).toUpperCase() + string.slice(1);
         }
 
-    }
+        function parseRSS(url, container) {
+            $.ajax({
+                url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent(url),
+                dataType: 'json'
+            })
+            .done(function (data) {
+                $(container).html('<h2>'+ data.responseData.feed.title +'</h2>');
+
+                $.each(data.responseData.feed.entries, function(key, value){
+                    var thehtml = '<h3><a href="'+value.link+'" target="_blank">'+value.title+'</a></h3>';
+                    $(container).append(thehtml);
+                });
+            })
+        }
 
 
-    parseRSS('http://www.actu-transport-logistique.fr/rss.xml', showArticles);
+        parseRSS('http://www.actu-transport-logistique.fr/rss.xml', "#site1");
 
-});
+    });
+
+}) ();
+
