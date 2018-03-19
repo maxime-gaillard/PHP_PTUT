03c2587650404ca0807fe431ec74d2e423a9b197
olivier <olivier.fesquet@etu.univ-amu.fr>
2018-03-19 17:32:32 +0100

    Modification affichage des publications privées ; css

--- controleur/publier.php
+++ controleur/publier.php
@@ -1,39 +1,62 @@
 <?php
 session_start();
+
+
 include '../modele/IncludeDB.php';
 $descitpion = htmlspecialchars($_POST['description']);
+
+// Where the file is going to be placed
+//$target_path = "../fichierjoint/";
+//
+//var_dump($_FILES);
+//die;
+
+/* Add the original filename to our target path.
+Result is "uploads/filename.extension" */
+//$target_path = $target_path . basename( $_FILES['userfile']['name']);
+
+
+
+//$target_path = $target_path . basename( $_FILES['uploadedfile']['name']);
+
+//echo '-->' . $target_path;
+//
+//
+//
+//if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
+//    echo "The file ".  basename( $_FILES['userfile']['name']).
+//        " has been uploaded";
+//} else{
+//    echo "There was an error uploading the file, please try again!";
+//}
+
+
+//$path = "../fichierjoint";
+//$path = $path . basename( $_FILES['userfile']['name']);
+//
+//echo "test";
+//
+//if(move_uploaded_file($_FILES['userfile']['tmp_name'], $path)) {
+//    echo "Success uploading". basename($_FILES['userfile']['name']);
+//} else {
+//    echo "Error when uploading file.";
+//}
+
 if($descitpion == "" || is_null($descitpion)) {
-//    echo 'HAHHAHAHAHAHHA';
-//    die();
 }
 
 $id = $_SESSION['NumInscrit'];
 
 if(is_null($_SESSION['NumInscrit'])) {
-//    echo "Vous n'etes pas connecte !";
-//    die();
 }
-//echo "TEST";
-//$req1 = $bdd->prepare('SELECT * FROM Chercheur WHERE NumC = :numC');
-//$req1->execute(array('numC' => $id,) ) or die (print_r($req->errorInfo())) ;
+
 
 $resultat = Chercheur::selectByNum($id);
 
 if ($resultat) {
     PublicationPublique::insertPublicationPublique($_POST['titre'], $_POST['date'], $_POST['description'], $id);
-//    $req = $bdd->prepare('INSERT INTO PublicationPublique (LibellePubl, NumInscrit) VALUES (:Libelle, :NumInscrit)');
-//    $req->execute(array(
-//        'Libelle' => $_POST['description'],
-//        'NumInscrit' => $id) ) or die (print_r($req->errorInfo())) ;
 } else {
     PublicationPrivee::insertPublicationPrivee($_POST['titre'], $_POST['description'], $id, $_POST['date']);
-//    $req = $bdd->prepare('INSERT INTO PublicationPrivee (LibellePriv, NumInscrit) VALUES (:Libelle, :NumInscrit)');
-//    $req->execute(array(
-//        'Libelle' => $_POST['description'],
-//        'NumInscrit' => $id,) ) or die (print_r($req->errorInfo())) ;
 }
 
-//echo 'L\'utilisateur ' . $id . ' a créé une nouvelle publication !';
-
-//$bdd = null;
 header('Location:../index.php');
